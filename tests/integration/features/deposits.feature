4b28580e49d242eb996814e7d8d11e24bf891faf
Ralph Ribeiro <ralph_ribeiro@msn.com>
2022-07-27 21:19:57 -0300

    Finalize deposits CRUD

--- tests/integration/features/deposits.feature
+++ tests/integration/features/deposits.feature
@@ -13,12 +13,54 @@ Feature: Deposits
     When make a request to post a valid deposit
     Then the status_code is "201"
     And a valid deposit is returned
+    And save the deposit id
+    And make request to delete a deposit by id
 
-  Scenario: Create deposit with items list
+  Scenario: Get deposit by id
     Given the test-bees url
     And a valid deposit
-    And a list of valid items
-    When make a request to post a valid deposit with items
+    When make a request to post a valid deposit
     Then the status_code is "201"
     And a valid deposit is returned
-    And with a list of valid items
+    When save the deposit id
+    And make a request to get deposit by id
+    Then the status_code is "200"
+    And a valid deposit is returned
+    And make request to delete a deposit by id
+
+  Scenario: Update deposit with patch
+    Given the test-bees url
+    And a valid deposit
+    When make a request to post a valid deposit
+    Then the status_code is "201"
+    And a valid deposit is returned
+    When save the deposit id
+    And make request to update a deposit values with "patch"
+    And save the deposit id
+    Then the status_code is "200"
+    And a valid deposit is returned
+    And deposit values has changed
+    And make request to delete a deposit by id
+
+  Scenario: Update deposit with put
+    Given the test-bees url
+    And a valid deposit
+    When make a request to post a valid deposit
+    Then the status_code is "201"
+    And a valid deposit is returned
+    When save the deposit id
+    And make request to update a deposit values with "put"
+    Then the status_code is "200"
+    And a valid deposit is returned
+    And deposit values has changed
+    And make request to delete a deposit by id
+
+  Scenario: Delete deposit
+    Given the test-bees url
+    And a valid deposit
+    When make a request to post a valid deposit
+    Then the status_code is "201"
+    And a valid deposit is returned
+    When save the deposit id
+    And make request to delete a deposit by id
+    Then the status_code is "204"
--- tests/integration/features/steps/deposits.py
+++ tests/integration/features/steps/deposits.py
@@ -3,7 +3,7 @@ import json
 from behave import step
 import requests
 
-
+from helpers.tools import check_if_changed_values
 from modules.deposits import (
     is_valid_deposits,
     is_valid_deposit,
@@ -33,13 +33,17 @@ def make_deposit(context):
 @step('make a request to post a valid deposit')
 def post_deposit(context):
     url = context.root_url + 'deposits.json'
-    headers = {
-        'accept': '*/*',
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
     context.response = requests.post(
         url, headers=headers, data=json.dumps(context.deposit)
     )
+
+
+@step('make a request to get deposit by id')
+def post_deposit_with_items(context):
+    url = f'{context.root_url}deposits/{context.deposit_id}.json'
+    context.response = requests.get(url)
+
 
 @step('a valid deposit is returned')
 def check_deposit(context):
@@ -47,3 +51,42 @@ def check_deposit(context):
     assert is_valid_deposit(deposit), (
         '\nInvalid deposit.'
     )
+
+
+@step('save the deposit id')
+def save_id(context):
+    context.deposit_id = context.response.json().get('id')
+
+
+@step('make request to update a deposit values with "{mode}"')
+def update_deposit(context, mode):
+    modes = {
+        'patch': requests.patch,
+        'put': requests.put
+    }
+    old_deposit_id = context.deposit_id
+    new_deposit = make_a_valid_deposit()
+    headers = {'Content-Type': 'application/json'}
+    url = f'{context.root_url}deposits/{old_deposit_id}.json'
+    context.response = modes[mode](
+        url, headers=headers, data=json.dumps(new_deposit)
+    )
+    context.new_deposit = new_deposit
+
+@step('deposit values has changed')
+def check_changed_values(context):
+    expected = context.new_deposit
+    returned = context.response.json()
+    assert check_if_changed_values(expected, returned), (
+        '\nThe deposit has not been updated'
+        f'\nExpected: {expected}'
+        f'\nReturned: {returned}'
+    )
+
+
+
+@step('make request to delete a deposit by id')
+def delete_deposit(context):
+    deposit_id = context.deposit_id
+    url = f'{context.root_url}deposits/{deposit_id}.json'
+    context.response = requests.delete(url)
