7982ca79141ced030ad4cd2993494496cbfa5b08
Ralph Ribeiro <ralph_ribeiro@msn.com>
2022-07-31 19:23:20 -0300

    Disable geckodriver.log

--- tests/UI/features/environment.py
+++ tests/UI/features/environment.py
@@ -1,3 +1,5 @@
+from os import devnull
+
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -6,7 +8,9 @@ def before_all(context):
     context.root_url = 'https://test-bees.herokuapp.com'
     driver_manager = GeckoDriverManager()
     context.driver = webdriver.Firefox(
-        executable_path=driver_manager.install())
+        executable_path=driver_manager.install(),
+        service_log_path=devnull
+    )
     context.driver.maximize_window()
 
 
